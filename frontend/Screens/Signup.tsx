7ff58ff74ab26b3bad67c927b7d6915bc038d4ad
colingraydon <94876972+colingraydon@users.noreply.github.com>
2023-07-09 15:53:57 -0400

    frontend skeleton written (#1)
    using expo. app wrapped in apollo client, backend calls working

---
+++ frontend/Screens/Signup.tsx
@@ -0,0 +1,26 @@
+import React, { useEffect, useState } from "react";
+import { View, Text, ScrollView } from "react-native";
+import { useGetUsersQuery } from "../src/generated/graphql";
+
+const Signup = () => {
+  const { data, loading } = useGetUsersQuery();
+
+  const [users, setUsers] = useState(Array<any>);
+
+  useEffect(() => {
+    if (data) {
+      setUsers(data.getUsers);
+    }
+  }, [data]);
+  return (
+    <ScrollView>
+      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+        <Text>This is a Signup Screen</Text>
+      </View>
+      {users.map((user, index) => (
+        <Text key={index}>{user.id}</Text>
+      ))}
+    </ScrollView>
+  );
+};
+export default Signup;
---
+++ frontend/src/nagivation/AppStack.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+interface AppStackProps {}
+
+const AppStack: React.FC<AppStackProps> = ({}) => {
+  return (
+    <View>
+      <Text>Placeholder App Stack</Text>
+    </View>
+  );
+};
+
+export default AppStack;
