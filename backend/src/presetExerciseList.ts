9921b25caf8684e65bb41ffb3d7e57712add7c1d
Colin Graydon <colingraydon@gmail.com>
2023-08-13 23:59:54 -0400

    added jest and started writing unit tests
    edited CI pipeline to run jest tests

--- backend/src/presetExerciseList.ts
+++ backend/src/presetExerciseList.ts
@@ -1,21 +1,30 @@
 import { MyExercise } from "./types";
 
-const bicepCurl: MyExercise = { name: "Bicep Curl", muscleGroup: ["Biceps"] };
+const bicepCurl: MyExercise = {
+  exerciseName: "Bicep Curl",
+  muscleGroup: ["Biceps"],
+};
 const hammerCurl: MyExercise = {
-  name: "Hammer Curl",
+  exerciseName: "Hammer Curl",
   muscleGroup: ["Biceps"],
 };
 const inclineCurl: MyExercise = {
-  name: "Incline Curl",
+  exerciseName: "Incline Curl",
   muscleGroup: ["Biceps"],
 };
 const declineCurl: MyExercise = {
-  name: "Decline Curl",
+  exerciseName: "Decline Curl",
   muscleGroup: ["Biceps"],
 };
 const preacherCurl: MyExercise = {
-  name: "Preacher Curl",
+  exerciseName: "Preacher Curl",
   muscleGroup: ["Biceps"],
 };
 
-export const presetExerciseList = <MyExercise[]>[bicepCurl];
+export const presetExerciseList = <MyExercise[]>[
+  bicepCurl,
+  hammerCurl,
+  inclineCurl,
+  declineCurl,
+  preacherCurl,
+];
