02770d09c35b51a4f001a7fca5065c3e3b888cca
Colin Graydon <colingraydon@gmail.com>
2023-08-24 21:51:05 -0400

    documentation added for CurrentWorkout.tsx
    code cleaned up, console logs removed

--- frontend/Screens/CurrentWorkout.tsx
+++ frontend/Screens/CurrentWorkout.tsx
@@ -18,6 +18,9 @@ import MyExerciseSearchBar from "../src/components/MyExerciseSearchBar";
 
 interface CurrentWorkoutProps {}
 
+/**
+ * ExerciseProps, for passing data to an exercise Component
+ */
 export interface ExerciseProps {
   exerciseName: string;
   myExerciseId: number;
@@ -28,18 +31,31 @@ export interface ExerciseProps {
   handleAddSet?: (index: number) => void;
 }
 
+/**
+ * Sets interface for storing sets in {@link ExerciseProps}
+ */
 export interface Sets {
   weight?: number;
   reps?: number;
   time?: number;
 }
 
+/**
+ * This screen shows the current workout. It is composed of child components,
+ * including a MyExercise search bar, Exercises, and Sets for each exercise
+ * @param object containing a route and navigation, defined in {@link Props}
+ * @returns the screen
+ */
 const CurrentWorkout: React.FC<CurrentWorkoutProps> = ({
   route,
   navigation,
 }: Props) => {
+  //state for list of exercises performed in this workout
   const [exercises, setExercises] = useState<Array<ExerciseProps>>(null);
-  const [workoutName, setWorkoutName] = useState<string>("");
+
+  const [workoutName, setWorkoutName] = useState<string>(""); //state for the workout name
+
+  //state for storing MyExercises, initialized to null. type from GetMyExercisesQuery
   const [myExercises, setMyExercises] = useState<
     Array<{
       __typename?: "MyExercises";
@@ -50,8 +66,8 @@ const CurrentWorkout: React.FC<CurrentWorkoutProps> = ({
     }>
   >(null);
 
-  const [service, setService] = useState<string>(""); //val for exercise dropdown
-  const [searchValue, setSearchValue] = useState<string>("");
+  const [service, setService] = useState<string>(""); //string of exercise name for exercise dropdown
+  const [searchValue, setSearchValue] = useState<string>(""); //myExerciseID from exercise dropdown, stored as string
 
   const { data: dataMyExercises, loading: loadingMyExercises } =
     useGetMyExercisesQuery();
@@ -75,32 +91,42 @@ const CurrentWorkout: React.FC<CurrentWorkoutProps> = ({
     setSearchValue(e);
   };
 
+  /**
+   * Handles adding an exercise to the workout and updates the state.
+   * @param exId id of the exercise as a string
+   */
   const handleAddExercise = (exId: string) => {
+    //finds the exercise in MyExercises with myExerciseId equal to exId.
     const searchedExercise = dataMyExercises.getMyExercises.find(
       (element) => element.myExerciseId === parseInt(exId)
     );
+    //adds a Set to that exercise with reps + weight 0
     const newExercise = { ...searchedExercise, sets: [{ reps: 0, weight: 0 }] };
     // const newExercise = { ...searchedExercise };
 
-    console.log("newExercise: ", newExercise);
+    //if no exercises yet, set exercises to be an array containing only this element
     if (!exercises) {
-      console.log("in !exercises");
       setExercises([newExercise]);
     } else {
+      //else spread the existing exercises and add the newExercise
       setExercises([...exercises, newExercise]);
     }
   };
 
+  /**
+   * Adds a set to an exercise. This function is passed to the child component, SingleExercise.
+   * @param index index in the array of exercises specifies the exercise to add a set to.
+   */
   const handleAddSet = (index: number) => {
     let newExerciseArr = exercises;
-    console.log("exercises before pushing: ", exercises);
+    //if there are no sets in exercises state
     if (newExerciseArr[index].sets === undefined) {
       newExerciseArr[index].sets = [{ reps: 0, weight: 0 }];
     } else {
+      //else push it onto the existing array of sets
       newExerciseArr[index].sets.push({ reps: 0, weight: 0 });
     }
-    setExercises(newExerciseArr);
-    console.log("Exercises after pushing: ", exercises);
+    setExercises(newExerciseArr); //sets the state of exercises with the newly added set
   };
 
   return (
